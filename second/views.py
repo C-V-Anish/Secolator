3710b3e27c19d44b703b1ae91ac4a3ed527aa6ef
C-V-Anish <mahadevrao77@gmail.com>
2022-04-15 01:24:22 +0530

    a

--- second/views.py
+++ second/views.py
@@ -7,23 +7,31 @@ def index(request):
 
 
 def Result(request):
-    djtext= request.GET.get('a', 'default')
-    djtext1= request.GET.get('ab', 'default')
+    djtext = request.GET.get('a', 'default')
+    djtext1 = request.GET.get('ab', 'default')
     c = request.GET.get('abc', 'default')
     try:
-        a=int(djtext)
-        b=int(djtext1)
-        if(c=="+"):
-            return HttpResponse(a+b)
-        elif(c=="-"):
-            return HttpResponse(a-b)
+        a = int(djtext)
+        b = int(djtext1)
+        if (c == "+"):
+            x = {'Purpose': 'Addition', 'Result': a + b}
+            return render(request, 'Result.html', x)
+        elif (c == "-"):
+            x = {'Purpose': 'Subtraction','Result': a - b}
+            return render(request, 'Result.html', x)
         elif (c == "*"):
-            return HttpResponse(a*b)
+            x = {'Purpose': 'Multiplication', 'Result': a * b}
+            return render(request, 'Result.html', x)
         elif (c == "/"):
-            return HttpResponse(a/b)
+            x = {'Purpose': 'Division', 'Result': a / b}
+            return render(request, 'Result.html', x)
         elif (c == "%"):
-            return HttpResponse(a%b)
+            x = {'Purpose': 'Remainder', 'Result': a % b}
+            return render(request, 'Result.html', x)
         elif (c == "^"):
-            return HttpResponse(a**b)
+            x = {'Purpose': 'Power', 'Result': a ** b}
+            return render(request, 'Result.html', x)
+        else :
+            return HttpResponse("Enter appropriate operator")
     except:
-        return HttpResponse('Inappropriate datatype...Please enter numbers')
+        return HttpResponse('Inappropriate datatype....Please enter carefully')
